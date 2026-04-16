156d75c0dce93831c31b96ea30cf9912d4207fd2
Kovvada Naga Venkata Sai Meghana <139636360+SaiMeghana14@users.noreply.github.com>
2026-04-16 17:32:52 +0530

    Create highlighter.py

---
+++ utils/highlighter.py
@@ -0,0 +1,14 @@
+def find_missing_info(ai_answer, real_data):
+    ai_words = set(ai_answer.lower().split())
+    real_words = set(real_data.lower().split())
+
+    missing = real_words - ai_words
+    return list(missing)[:15]
+
+
+def find_overlap(ai_answer, real_data):
+    ai_words = set(ai_answer.lower().split())
+    real_words = set(real_data.lower().split())
+
+    overlap = ai_words.intersection(real_words)
+    return list(overlap)[:15]

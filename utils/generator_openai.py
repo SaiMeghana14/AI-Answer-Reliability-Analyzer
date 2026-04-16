6b4ef98601d61c4b02bd3982705bb39d0daf7f84
Kovvada Naga Venkata Sai Meghana <139636360+SaiMeghana14@users.noreply.github.com>
2026-04-16 18:06:31 +0530

    Update generator_openai.py

--- utils/generator_openai.py
+++ utils/generator_openai.py
@@ -2,7 +2,7 @@ import openai
 
 openai.api_key = "YOUR_API_KEY"
 
-def generate_answer(question):
+def generate_openai_answer(question):
     response = openai.ChatCompletion.create(
         model="gpt-4o-mini",
         messages=[{"role": "user", "content": question}]

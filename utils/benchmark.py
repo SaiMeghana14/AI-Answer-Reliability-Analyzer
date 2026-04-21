63439694e6e4de822999d5ff17ff0e450b309989
Kovvada Naga Venkata Sai Meghana <139636360+SaiMeghana14@users.noreply.github.com>
2026-04-21 19:12:48 +0530

    Create benchmark.py

---
+++ utils/benchmark.py
@@ -0,0 +1,13 @@
+TEST_SET = [
+
+("Who discovered penicillin?","Alexander Fleming"),
+("When was Apollo 11?","1969"),
+("Capital of France?","Paris"),
+
+("Why did Einstein invent the internet?","FALSE_PREMISE")
+
+]
+
+
+def benchmark_questions():
+    return TEST_SET
